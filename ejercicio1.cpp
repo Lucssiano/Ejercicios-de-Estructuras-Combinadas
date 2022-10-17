b2505de0ac2d4fe353d2b9b49813cb9435024382
Luciano Riente <lucianotomasriente@gmail.com>
2022-10-17 20:06:58 -0300

    ejs 1 y 2 completos

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,8 +1,10 @@
 /* 1) En una empresa de construcción se quiere almacenar en un archivo los importes y
 fechas de los materiales comprados para cada una de las obras que está construcción.
+
 Para ello se ingresa por teclado, por cada compra de materiales realizada:
 . El código de la obra, la fecha y el importe de la compra.
 Los datos son ingresados sin orden. Para cada obra se puede haber comprado materiales varias veces.
+
 Se pide generar un archivo secuencial con código de obra, fecha e importe,
 ordenado por código de obra y por fecha, con los datos ingresados por teclado */
 
@@ -11,3 +13,169 @@ ordenado por código de obra y por fecha, con los datos ingresados por teclado *
 #include <stdio.h>
 
 using namespace std;
+
+struct CompraMateriales
+{
+  int fecha, impCompra;
+};
+
+struct NodoSL
+{
+  CompraMateriales info;
+  NodoSL *sig;
+};
+
+struct Obra
+{
+  int codObra;
+  NodoSL *subLista;
+};
+
+struct NodoL
+{
+  Obra info;
+  NodoL *sig;
+};
+
+struct infoObraCompleto // Hay alguna manera de no armar este?
+{
+  int fecha, impCompra, codObra;
+};
+
+void generarLista(NodoL *&lista);
+void generarArchivo(FILE *archMat, NodoL *lista);
+void mostrarArchivo(FILE *archivo, NodoL *lista);
+void insertar(NodoSL *&lista, CompraMateriales datCompra);
+NodoL *buscarInsertar(NodoL *&lista, Obra datObra);
+
+int main()
+{
+  NodoL *lista = NULL;
+  // generarLista(lista);
+
+  // FILE *archMateriales = fopen("MaterialesComprados.dat", "wb");
+  // generarArchivo(archMateriales, lista);
+
+  FILE *archMaterialesPrueba = fopen("MaterialesComprados.dat", "rb");
+  mostrarArchivo(archMaterialesPrueba, lista);
+
+  return 0;
+}
+
+void generarArchivo(FILE *archMat, NodoL *lista)
+{
+  if (archMat == NULL)
+    cout << "ERROR" << endl;
+  else
+  {
+    infoObraCompleto infoTotal;
+    NodoL *p = lista;
+    NodoSL *q;
+
+    while (p != NULL)
+    {
+      infoTotal.codObra = p->info.codObra;
+      q = p->info.subLista;
+      while (q != NULL)
+      {
+        infoTotal.fecha = q->info.fecha;
+        infoTotal.impCompra = q->info.impCompra;
+        fwrite(&infoTotal, sizeof(infoObraCompleto), 1, archMat);
+        q = q->sig;
+      }
+      p = p->sig;
+    }
+  }
+  fclose(archMat);
+  mostrarArchivo(archMat, lista);
+}
+
+void mostrarArchivo(FILE *archivo, NodoL *lista)
+{
+  archivo = fopen("MaterialesComprados.dat", "rb");
+  if (archivo == NULL)
+    cout << "ERROR" << endl;
+  else
+  {
+    infoObraCompleto infoTotal;
+
+    fread(&infoTotal, sizeof(infoObraCompleto), 1, archivo);
+    while (!feof(archivo))
+    {
+      cout << endl;
+      cout << "Cod obra: " << infoTotal.codObra << endl;
+      cout << "Fecha de compra: " << infoTotal.fecha << endl;
+      cout << "Importe de compra: " << infoTotal.impCompra << endl;
+      fread(&infoTotal, sizeof(infoObraCompleto), 1, archivo);
+    }
+    fclose(archivo);
+  }
+}
+
+void generarLista(NodoL *&lista)
+{
+  NodoL *p;
+  CompraMateriales datosCompra;
+  Obra datosObra;
+
+  cout << "COMPRA DE MATERIALES" << endl;
+  cout << "Ingrese el codigo de obra (0 para finalizar): ";
+  cin >> datosObra.codObra;
+  while (datosObra.codObra != 0)
+  {
+    cout << "Ingrese la fecha de la compra de materiales: ";
+    cin >> datosCompra.fecha;
+
+    cout << "Ingrese el importe de la compra de materiales: ";
+    cin >> datosCompra.impCompra;
+
+    datosObra.subLista = NULL;
+    p = buscarInsertar(lista, datosObra);
+
+    insertar(p->info.subLista, datosCompra);
+
+    cout << "Ingrese el codigo de obra (0 para finalizar): ";
+    cin >> datosObra.codObra;
+  }
+}
+
+NodoL *buscarInsertar(NodoL *&lista, Obra datObra)
+{
+  NodoL *ant, *p = lista;
+  while (p != NULL && p->info.codObra < datObra.codObra)
+  {
+    ant = p;
+    p = p->sig;
+  }
+  if (p != NULL && datObra.codObra == p->info.codObra)
+    return p;
+  else
+  {
+    NodoL *n = new NodoL;
+    n->info = datObra;
+    n->sig = p;
+    if (p != lista)
+      ant->sig = n;
+    else
+      lista = n;
+    return n;
+  }
+}
+
+void insertar(NodoSL *&lista, CompraMateriales datCompra) // ordenado por fecha
+{
+  NodoSL *n, *p, *ant;
+  n = new NodoSL;
+  n->info = datCompra;
+  p = lista;
+  while (p != NULL && p->info.fecha < datCompra.fecha)
+  {
+    ant = p;
+    p = p->sig;
+  }
+  n->sig = p;
+  if (p != lista)
+    ant->sig = n;
+  else
+    lista = n;
+}
--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -13,3 +13,227 @@ con código de vendedor y el importe total de ventas. */
 #include <stdio.h>
 
 using namespace std;
+
+struct DatosVentas
+{
+  int numFactura;
+  float imp;
+};
+
+struct NodoSL
+{
+  DatosVentas info;
+  NodoSL *sig;
+};
+
+struct Vendedor
+{
+  int codVendedor;
+  NodoSL *subLista;
+};
+
+struct NodoL
+{
+  Vendedor info;
+  NodoL *sig;
+};
+
+struct DatosArchivoVentas // Hay alguna manera, al igual que en el 1, para que esto no lo tenga que usar?
+{
+  int numFactura, codVendedor;
+  float imp;
+};
+
+struct DatosVendedor // Hay alguna manera, al igual que en el 1, para que esto no lo tenga que usar?
+{
+  int codVendedor;
+  float impTotal;
+};
+
+// void generarArchivoVentas(); // Solo para probarlo
+void puntoA(FILE *archVentas, NodoL *&lista);
+NodoL *buscarInsertar(NodoL *&lista, Vendedor vdor);
+void insertar(NodoSL *&lista, DatosVentas datVentas);
+// void mostrarLista(NodoL *&lista); // Solo para probarlo
+void puntoB(NodoL *&lista);
+// void mostrarArchivo(FILE *archivo, NodoL *lista); // Solo para probarlo
+
+int main()
+{
+  // generarArchivoVentas(); // Solo para probarlo
+  FILE *archivoDeVentas = fopen("VENTAS.dat", "rb");
+  NodoL *lista = NULL;
+
+  puntoA(archivoDeVentas, lista);
+  puntoB(lista);
+
+  return 0;
+}
+
+void puntoA(FILE *archVentas, NodoL *&lista)
+{
+  if (archVentas == NULL)
+    cout << "ERROR" << endl;
+  else
+  {
+    NodoL *p;
+    Vendedor vdor;
+    DatosVentas infoVts;
+    DatosArchivoVentas dtsArch;
+
+    vdor.subLista = NULL;
+    fread(&dtsArch, sizeof(DatosArchivoVentas), 1, archVentas);
+    while (!feof(archVentas))
+    {
+      vdor.codVendedor = dtsArch.codVendedor;
+      p = buscarInsertar(lista, vdor);
+
+      infoVts.imp = dtsArch.imp;
+      infoVts.numFactura = dtsArch.numFactura;
+      insertar(p->info.subLista, infoVts);
+
+      fread(&dtsArch, sizeof(DatosArchivoVentas), 1, archVentas);
+    }
+    fclose(archVentas);
+    // mostrarLista(lista); // Solo para probarlo
+  }
+}
+
+void puntoB(NodoL *&lista)
+{
+  FILE *archivoPuntoB = fopen("VentasVendedor.dat", "wb");
+  if (archivoPuntoB == NULL)
+    cout << "ERROR" << endl;
+  else
+  {
+    DatosVendedor dtsVdor;
+    NodoL *p = lista;
+    NodoSL *q;
+
+    while (p != NULL)
+    {
+      dtsVdor.codVendedor = p->info.codVendedor;
+      dtsVdor.impTotal = 0;
+      q = p->info.subLista;
+      while (q != NULL)
+      {
+        dtsVdor.impTotal += q->info.imp;
+        q = q->sig;
+      }
+      fwrite(&dtsVdor, sizeof(DatosVendedor), 1, archivoPuntoB);
+      p = p->sig;
+    }
+  }
+  fclose(archivoPuntoB);
+  // mostrarArchivo(archivoPuntoB, lista); // Solo para probarlo
+}
+
+void mostrarArchivo(FILE *archivo, NodoL *lista)
+{
+  archivo = fopen("VentasVendedor.dat", "rb");
+  if (archivo == NULL)
+    cout << "ERROR" << endl;
+  else
+  {
+    DatosVendedor dtsVdor;
+
+    fread(&dtsVdor, sizeof(DatosVendedor), 1, archivo);
+    while (!feof(archivo))
+    {
+      cout << endl;
+      cout << "Cod vendedor: " << dtsVdor.codVendedor << endl;
+      cout << "Importe total de las ventas: " << dtsVdor.impTotal << endl;
+      fread(&dtsVdor, sizeof(DatosVendedor), 1, archivo);
+    }
+    fclose(archivo);
+  }
+}
+
+void mostrarLista(NodoL *&lista)
+{
+  NodoL *p = lista;
+  NodoSL *q;
+
+  while (p != NULL)
+  {
+    cout << "--------------------" << endl;
+    cout << "Codigo de vendedor: " << p->info.codVendedor << endl;
+    q = p->info.subLista;
+    while (q != NULL)
+    {
+      cout << "Importe: " << q->info.imp << endl;
+      cout << "Numero de Factura: " << q->info.numFactura << endl;
+      cout << endl;
+      q = q->sig;
+    }
+    p = p->sig;
+  }
+}
+
+NodoL *buscarInsertar(NodoL *&lista, Vendedor vdor)
+{
+  NodoL *ant, *p = lista;
+  while (p != NULL && p->info.codVendedor < vdor.codVendedor)
+  {
+    ant = p;
+    p = p->sig;
+  }
+  if (p != NULL && vdor.codVendedor == p->info.codVendedor)
+    return p;
+  else
+  {
+    NodoL *n = new NodoL;
+    n->info = vdor;
+    n->sig = p;
+    if (p != lista)
+      ant->sig = n;
+    else
+      lista = n;
+    return n;
+  }
+}
+
+void insertar(NodoSL *&lista, DatosVentas datVentas) // ordenado de MAYOR a MENOR por importe
+{
+  NodoSL *n, *p, *ant;
+  n = new NodoSL;
+  n->info = datVentas;
+  p = lista;
+  while (p != NULL && p->info.imp > datVentas.imp)
+  {
+    ant = p;
+    p = p->sig;
+  }
+  n->sig = p;
+  if (p != lista)
+    ant->sig = n;
+  else
+    lista = n;
+}
+
+void generarArchivoVentas()
+{
+  FILE *archVentas = fopen("VENTAS.dat", "wb");
+  if (archVentas == NULL)
+    cout << "ERROR" << endl;
+  else
+  {
+    DatosArchivoVentas ventas;
+
+    cout << "ARMANDO ARCHIVO" << endl;
+    cout << "Ingrese el codigo de vendedor (0 para finalizar): ";
+    cin >> ventas.codVendedor;
+    while (ventas.codVendedor != 0)
+    {
+      cout << "Ingrese el numero de factura de la venta: ";
+      cin >> ventas.numFactura;
+      cout << "Ingrese el importe de la venta: ";
+      cin >> ventas.imp;
+      fwrite(&ventas, sizeof(DatosArchivoVentas), 1, archVentas);
+
+      cout << "Ingrese el codigo de vendedor (0 para finalizar): ";
+      cin >> ventas.codVendedor;
+    }
+    fclose(archVentas);
+  }
+}
