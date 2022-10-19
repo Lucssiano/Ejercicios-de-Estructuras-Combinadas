5bb43e866658030e7564966f91a8fc3030734918
Luciano Riente <lucianotomasriente@gmail.com>
2022-10-19 18:48:50 -0300

    hasta ej 4 sin el 3

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,10 +1,91 @@
 /* 3) Idem ejercicio 2, en el comercio hay 10 vendedores cuyo código de vendedor es un número
 del 1 al 10 */
 
+/* Dado el archivo VENTAS.DAT, ordenado por número de factura, con la siguiente
+estructura de registro:
+Número de factura (int)
+Código de vendedor (int) (1 a 10)
+Importe (float)
+a) Imprima un listado ordenado por código de vendedor, con código de vendedor
+y el detalle de cada venta, ordenadas de mayor a menor por importe.
+b) Genere el archivo “VentasVendedor.dat” ordenado por código de vendedor,
+con código de vendedor y el importe total de ventas. */
+
+// Un vector de punteros que cada posicion apunta a una lista por cada vendedor
+
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
 
 using namespace std;
 
-// No entiendo en que cambia
+struct DatosVentas
+{
+  int numFactura;
+  float imp;
+};
+
+struct NodoL
+{
+  int codVendedor[10];
+  NodoL *sig;
+};
+
+// struct DatosArchivoVentas // Hay alguna manera, al igual que en el 1, para que esto no lo tenga que usar?
+// {
+//   int numFactura, codVendedor;
+//   float imp;
+// };
+
+// struct DatosVendedor // Hay alguna manera, al igual que en el 1, para que esto no lo tenga que usar?
+// {
+//   int codVendedor;
+//   float impTotal;
+// };
+
+// void generarArchivoVentas(); // Solo para probarlo
+void puntoA(FILE *archVentas, NodoL *&lista);
+// NodoL *buscarInsertar(NodoL *&lista, Vendedor vdor);
+// void insertar(NodoSL *&lista, DatosVentas datVentas);
+// void mostrarLista(NodoL *&lista); // Solo para probarlo
+// void puntoB(NodoL *&lista);
+// void mostrarArchivo(FILE *archivo, NodoL *lista); // Solo para probarlo
+
+int main()
+{
+  // generarArchivoVentas(); // Solo para probarlo
+  FILE *archivoDeVentas = fopen("VENTAS.dat", "rb");
+  NodoL *lista = NULL;
+  puntoA(archivoDeVentas, lista);
+
+  return 0;
+}
+
+// void puntoA(FILE *archVentas, NodoL *&lista)
+// {
+//   if (archVentas == NULL)
+//     cout << "ERROR" << endl;
+//   else
+//   {
+//     NodoL *p;
+//     Vendedor vdor;
+//     DatosVentas infoVts;
+//     DatosArchivoVentas dtsArch;
+
+//     vdor.subLista = NULL;
+//     fread(&dtsArch, sizeof(DatosArchivoVentas), 1, archVentas);
+//     while (!feof(archVentas))
+//     {
+//       vdor.codVendedor = dtsArch.codVendedor;
+//       p = buscarInsertar(lista, vdor);
+
+//       infoVts.imp = dtsArch.imp;
+//       infoVts.numFactura = dtsArch.numFactura;
+//       insertar(p->info.subLista, infoVts);
+
+//       fread(&dtsArch, sizeof(DatosArchivoVentas), 1, archVentas);
+//     }
+//     fclose(archVentas);
+//     // mostrarLista(lista); // Solo para probarlo
+//   }
+// }
--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -23,79 +23,227 @@ c) Informar en qué actividad deportiva hubo más de un primer puesto. */
 
 using namespace std;
 
+#define codigoActividad 15
+#define meses 12
+
 struct Fecha
 {
   int dia, mes, año;
 };
 
-struct DatosCompetidores
+struct DatosArchivo
 {
   int codActividad, numSocio, resultado;
   Fecha fechaComp;
 };
 
+struct DatosListaCompetidores
+{
+  int numSocio, resultado;
+};
+
+struct NodoL
+{
+  DatosListaCompetidores info;
+  NodoL *sig;
+};
+
 // void generarArchivoCompetidores(); // Solo para probarlo
-void puntoA(FILE *archComp);
-void puntoB();
-void puntoC();
+void inicializarMatriz(NodoL *mt[codigoActividad][meses], int m, int n);
+void puntoA(FILE *archComp, NodoL *mt[codigoActividad][meses]);
+void insertar(NodoL *&lista, DatosListaCompetidores competidor);
+void listar(NodoL *lista);
+void mostrarPorColumna(NodoL *mt[codigoActividad][meses], int m, int n);
+void puntoB(NodoL *mt[codigoActividad][meses], int m, int n);
+int totalDeAtletasPorActividad(NodoL *lista);
+void puntoC(NodoL *mt[codigoActividad][meses], int m, int n);
+int totalPrimerPuesto(NodoL *lista);
 
 int main()
 {
   // generarArchivoCompetidores(); // Solo para probarlo
   FILE *archivoDeCompetidores = fopen("Competidores.dat", "rb");
 
-  puntoA(archivoDeCompetidores);
-  puntoB();
-  puntoC();
-
-  return 0;
-}
-
-/* Informar por cada actividad, por cada mes, cada uno de los atletas que
-compitieron y el resultado obtenido, ordenado por código de actividad, por
-mes y por número de socio. */
-void puntoA(FILE *archComp)
-{
-  if (archComp == NULL)
+  if (archivoDeCompetidores == NULL)
     cout << "ERROR" << endl;
   else
   {
+    NodoL *matrizActividades[codigoActividad][meses]; // Matriz de listas
 
-    fclose(archComp);
+    inicializarMatriz(matrizActividades, codigoActividad, meses);
+
+    puntoA(archivoDeCompetidores, matrizActividades);
+    fclose(archivoDeCompetidores);
+    puntoB(matrizActividades, codigoActividad, meses);
+    puntoC(matrizActividades, codigoActividad, meses);
+  }
+  return 0;
+}
+
+void puntoA(FILE *archComp, NodoL *mt[codigoActividad][meses])
+{
+  DatosArchivo datosArch;
+
+  fread(&datosArch, sizeof(DatosArchivo), 1, archComp);
+  while (!feof(archComp))
+  {
+    NodoL *listaALlenar = mt[datosArch.codActividad - 1][datosArch.fechaComp.mes - 1];
+    DatosListaCompetidores datosLista;
+
+    datosLista.numSocio = datosArch.numSocio;
+    datosLista.resultado = datosArch.resultado;
+    insertar(listaALlenar, datosLista);
+    mt[datosArch.codActividad - 1][datosArch.fechaComp.mes - 1] = listaALlenar;
+
+    fread(&datosArch, sizeof(DatosArchivo), 1, archComp);
+  }
+  mostrarPorColumna(mt, codigoActividad, meses);
+}
+
+void puntoB(NodoL *mt[codigoActividad][meses], int m, int n) // Mostrar por columna de matrices
+{
+  for (int c = 0; c < n; c++)
+  {
+    cout << "-------------------------------" << endl;
+    cout << "Mes " << c + 1 << endl;
+    for (int f = 0; f < m; f++)
+    {
+      if (mt[f][c] != NULL)
+      {
+        cout << endl;
+        cout << "Codigo de actividad " << f + 1 << endl;
+        cout << "Total de atletas: " << totalDeAtletasPorActividad(mt[f][c]);
+      }
+    }
+    cout << endl;
   }
 }
 
-void puntoB()
+int totalDeAtletasPorActividad(NodoL *lista) // Es como la funcion listar
 {
+  NodoL *p;
+  p = lista;
+  int totAtletas = 0;
+  while (p != NULL)
+  {
+    totAtletas++;
+    p = p->sig;
+  }
+  return totAtletas;
 }
 
-void puntoC()
+void puntoC(NodoL *mt[codigoActividad][meses], int m, int n) // Mostrar por fila de matrices
 {
+  for (int f = 0; f < m; f++)
+  {
+    int cantPrimerPuesto = 0;
+    for (int c = 0; c < n; c++)
+    {
+      if (totalPrimerPuesto(mt[f][c]) >= 1)
+        cantPrimerPuesto++;
+    }
+    if (cantPrimerPuesto > 1)
+      cout << "Codigo de actividad " << f + 1 << " tiene " << cantPrimerPuesto << " primer puesto" << endl;
+  }
 }
 
-// void generarArchivoCompetidores()
-// {
-//   FILE *archVentas = fopen("Competidores.dat", "wb");
-//   if (archVentas == NULL)
-//     cout << "ERROR" << endl;
-//   else
-//   {
-//     DatosArchivoVentas ventas;
+int totalPrimerPuesto(NodoL *lista) // Es como la funcion listar
+{
+  NodoL *p;
+  p = lista;
+  int cantPrimerPuesto = 0;
+  while (p != NULL)
+  {
+    if (p->info.resultado == 1)
+      cantPrimerPuesto++;
+    p = p->sig;
+  }
+  return cantPrimerPuesto;
+}
 
-//     cout << "ARMANDO ARCHIVO" << endl;
-//     cout << "Ingrese el codigo de vendedor (0 para finalizar): ";
-//     cin >> ventas.codVendedor;
-//     while (ventas.codVendedor != 0)
-//     {
-//       cout << "Ingrese el numero de factura de la venta: ";
-//       cin >> ventas.numFactura;
-//       cout << "Ingrese el importe de la venta: ";
-//       cin >> ventas.imp;
-//       fwrite(&ventas, sizeof(DatosArchivoVentas), 1, archVentas);
+void inicializarMatriz(NodoL *mt[codigoActividad][meses], int m, int n)
+{
+  for (int f = 0; f < m; f++)
+    for (int c = 0; c < n; c++)
+      mt[f][c] = NULL;
+}
 
-//       cout << "Ingrese el codigo de vendedor (0 para finalizar): ";
-//       cin >> ventas.codVendedor;
-//     }
-//     fclose(archVentas);
-//   }
-// }
+void mostrarPorColumna(NodoL *mt[codigoActividad][meses], int m, int n)
+{
+  for (int c = 0; c < n; c++)
+  {
+    cout << "Mes " << c + 1 << endl;
+    for (int f = 0; f < m; f++)
+    {
+      if (mt[f][c] != NULL)
+      {
+        cout << endl;
+        cout << "Codigo de actividad " << f + 1 << endl;
+        listar(mt[f][c]); // mostrar de menor a mayor los codigo de actividad
+      }
+    }
+    cout << endl;
+  }
+}
+
+void listar(NodoL *lista)
+{
+  NodoL *p;
+  p = lista;
+  while (p != NULL)
+  {
+    cout << "Numero de socio: " << p->info.numSocio << endl;
+    cout << "Resultado: " << p->info.resultado << endl;
+    p = p->sig;
+  }
+}
+
+void insertar(NodoL *&lista, DatosListaCompetidores competidor)
+{
+  NodoL *q, *p, *ant;
+  q = new NodoL;
+  q->info = competidor;
+  p = lista;
+  while (p != NULL && p->info.numSocio < competidor.numSocio)
+  {
+    ant = p;
+    p = p->sig;
+  }
+  q->sig = p;
+  if (p != lista)
+    ant->sig = q;
+  else
+    lista = q;
+}
+
+void generarArchivoCompetidores()
+{
+  FILE *archCompetidores = fopen("Competidores.dat", "ab");
+  if (archCompetidores == NULL)
+    cout << "ERROR" << endl;
+  else
+  {
+    DatosArchivo competidores;
+
+    cout << "ARMANDO ARCHIVO" << endl;
+    cout << "Ingrese el codigo de actividad del 1 al 15 (0 para finalizar): ";
+    cin >> competidores.codActividad;
+    while (competidores.codActividad != 0)
+    {
+      cout << "Ingrese el numero de socio del competidor: ";
+      cin >> competidores.numSocio;
+      cout << "Ingrese el resultado del competidor: ";
+      cin >> competidores.resultado;
+      cout << "Ingrese el mes de la competencia: ";
+      cin >> competidores.fechaComp.mes;
+
+      // faltaria agregar bien la fecha
+
+      fwrite(&competidores, sizeof(DatosArchivo), 1, archCompetidores);
+
+      cout << "Ingrese el codigo de actividad del 1 al 15 (0 para finalizar): ";
+      cin >> competidores.codActividad;
+    }
+    fclose(archCompetidores);
+  }
+}
